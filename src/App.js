28ac455816d2b31288374ff0fb7992f788199ed5
Amin Al Asri <amin.el.asery@gmail.com>
2023-11-18 23:57:27 -0500

    step2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { database } from './firebase/firebaseConfig';
 import { ref, onValue } from 'firebase/database';
+import { Container, Grid, Card, CardContent, Typography, TextField } from '@mui/material';
 
 function App() {
   const [data, setData] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    console.log("useEffect called"); // Log when useEffect is called
     const dataRef = ref(database, '1p4rz_7ShnWM-EuNAqSs3k9aYCDFmGbh6giqAP6PpQIc/Sheet1');
-    const unsubscribe = onValue(dataRef, (snapshot) => {
-      const fetchedData = snapshot.val();
-      console.log("Fetched data:", fetchedData); // Log the fetched data
-      setData(fetchedData);
-    }, (error) => {
-      console.error("Firebase error:", error); // Log any errors
+    onValue(dataRef, (snapshot) => {
+      setData(snapshot.val());
     });
-  
-    return () => unsubscribe();
   }, []);
-  
+
+  function filterData(data) {
+    if (!searchQuery) return Object.entries(data);
+    return Object.entries(data).filter(([key, value]) => {
+      return Object.values(value).some(item =>
+        item.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    });
+  }
 
   if (!data) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>Data from Realtime Database</h1>
-      {data && Object.entries(data).map(([key, value]) => (
-        <div key={key}>
-          <strong>{key}:</strong> {JSON.stringify(value)}
-        </div>
-      ))}
-    </div>
+    <Container maxWidth="md" style={{ marginTop: '100px' }}>
+      <TextField
+        fullWidth
+        label="Search"
+        variant="outlined"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        style={{ marginBottom: '20px' }}
+      />
+      <h1>Student Info</h1>
+      <Grid container spacing={3}>
+        {filterData(data).map(([key, value]) => (
+          <Grid item xs={12} sm={6} md={4} key={key}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  {value.Name}
+                </Typography>
+                <Typography color="textSecondary">
+                  Degree: {value.Degree}
+                </Typography>
+                <Typography color="textSecondary">
+                  Graduation Date: {value['Gradaution Date']}
+                </Typography>
+                <Typography color="textSecondary">
+                  Job Type: {value['Job type']}
+                </Typography>
+                <Typography color="textSecondary">
+                  ID: {value.Id}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
-  
 }
 
 export default App;
